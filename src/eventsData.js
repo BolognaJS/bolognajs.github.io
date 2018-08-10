eb07bfcea35ef81735d44d1ebc04ef9367d333ab
Tomma5o <tptommy1992@gmail.com>
2018-08-10 17:50:51 +0200

    refactoring structure of files and moved data into a JSON file ;)

---
+++ src/eventsData.js
@@ -0,0 +1,182 @@
+/**
+ * @type next
+ * @description for the upcoming eveny
+ * 
+ * @type placeholder
+ * @description when there isn't planned any event and you want to type a placeholder cell
+ * 
+ * @type hands-on
+ * @todo
+ */
+
+define(function () {
+    return [
+        {
+            "date": "",
+            "type": ["next", "placeholder"],
+            "talks": [
+                {
+                    "title": "Stiamo organizzando il prossimo evento. Stay tuned",
+                    "speakers": [""]
+                }
+            ]
+        },
+        {
+            "date": "17 luglio 2018",
+            "type": ["hands-on"],
+            "talks": [
+                {
+                    "title": "Hands-on session: Serverless - Build e deploy di un’applicazione node.js su AWS",
+                    "speakers": ["Davide Fiorello"]
+                }
+            ]
+        },
+        {
+            "date": "25 giugno 2018",
+            "type": "hands-on",
+            "talks": [
+                {
+                    "title": "Hands-on session: React & TypeScript",
+                    "speakers": ["Alessandro Garzaro", "Francesco Bedussi"]
+                }
+            ]
+        },
+        {
+            "date": "23 maggio 2018",
+            "talks": [
+                {
+                    "title": "Atomic UI & Storybook",
+                    "speakers": ["Gabriele Mantovani[https://twitter.com/mantovanig_it]"]
+                },
+                {
+                    "title": "Css Love JS",
+                    "speakers": ["Vincenzo Innocenti Sedili"]
+                }
+            ]
+        },
+        {
+            "date": "18 aprile 2018",
+            "talks": [
+                {
+                    "title": "Web Components: from zero to hero",
+                    "speakers": ["Salvatore Laisa", "Alessio Occhipinti"]
+                }
+            ]
+        },
+        {
+            "date": "21 marzo 2018",
+            "type": "double",
+            "talks": [
+                {
+                    "title": "Vue.js, un'alternativa dinamica a JQuery",
+                    "speakers": ["Filippo Nardi", "Edo Gargano"]
+                },
+                {
+                    "title": "Risiko ReactJS: strategie d’invasione. Come proporre un'architettura headless basata su React JS in una grande azienda.",
+                    "speakers": ["Giorgio Borelli"]
+                }
+            ]
+        },
+        {
+            "date": "21 febbraio 2018",
+            "talks": [
+                {
+                    "title": "Pillole di JS",
+                    "speakers": ["Francesco Bedussi"]
+                },
+                {
+                    "title": "ES6 e Service Worker per lanciare un’applicazione web senza un bundler",
+                    "speakers": ["Edoardo Cavazza"]
+                }
+            ]
+        },
+        {
+            "date": "24 Gennaio 2018",
+            "talks": [
+                {
+                    "title": "ES2015 class: Oltre lo zucchero sintattico.",
+                    "speakers": ["Bruno Scopelliti"]
+                },
+                {
+                    "title": "Building Resilient Microservice APIs with REST and API Gateway",
+                    "speakers": ["Vincenzo Chianese"]
+                }
+            ]
+        },
+        {
+            "date": "29 novembre 2018",
+            "talks": [
+                {
+                    "title": "Node + React = GatsbyJS",
+                    "speakers": ["Giorgio Aquino[https://twitter.com/g100g]"]
+                },
+                {
+                    "title": "GraphQL, introduzione pratica",
+                    "speakers": ["Davide Fiorello"]
+                }
+            ]
+        },
+        {
+            "date": "8 Novembre 2017",
+            "type": "double",
+            "talks": [
+                {
+                    "title": "ES6: Intro to destructuring",
+                    "speakers": ["Giorgio Aquino[https://twitter.com/g100g]"]
+                },
+                {
+                    "title": "What is `this`?",
+                    "speakers": ["Francesco Bedussi"]
+                },
+                {
+                    "title": "When `not` use arrow functions",
+                    "speakers": ["Marco Patierno"]
+                },
+                {
+                    "title": "Create objects in JavaScript",
+                    "speakers": ["Vincenzo Innocenti Sedili"]
+                }
+            ]
+        },
+        {
+            "date": "30 Agosto 2017",
+            "type": "hands-on",
+            "talks": [
+                {
+                    "title": "Hands-on session: JS & Functional Programming",
+                    "speakers": ["Davide Fiorello"]
+                }
+            ]
+        },
+        {
+            "date": "02 Agosto 2017",
+            "type": "hands-on",
+            "talks": [
+                {
+                    "title": "Hands-on session: Jest",
+                    "speakers": ["Davide Fiorello"]
+                }
+            ]
+        },
+        {
+            "date": "04 Luglio 2017",
+            "type": "hands-on",
+            "talks": [
+                {
+                    "title": "Hands-on session: Redux",
+                    "speakers": ["Davide Fiorello"]
+                }
+            ]
+        },
+        {
+            "date": "13 Giugno 2017",
+            "type": "hands-on",
+            "talks": [
+                {
+                    "title": "Hands-on session: React",
+                    "speakers": ["Davide Fiorello"]
+                }
+            ]
+        }
+    ]
+});
